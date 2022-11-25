5a766c76f93e193a1939de61778255f7eb38dfb9
Lagaras Stelios <50661518+lagamura@users.noreply.github.com>
2022-11-25 12:42:28 +0200

    geo-frontend

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <Suspense>
+    <template #default>
+      <MainComp />
+    </template>
+    <template #fallback>
+      <span>Loading...</span>
+    </template>
+  </Suspense>
+</template>
+
+<script setup lang="ts">
+import MainComp from "./components/MainComp.vue";
+</script>
+
+<style scoped></style>
---
+++ src/components/MainComp.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Welcome to GeoChess</h1>
+    <!-- <div v-for="Tour of Tours" :key="Tour.TourName">
+        {{ Tour.TourName }}
+      </div> -->
+    {{ Tours }}
+  </div>
+</template>
+
+<script setup lang="ts">
+const VITE_BASE_URL = import.meta.env.VITE_BASE_URL;
+
+const response = await fetch(VITE_BASE_URL + "/tournaments", {
+  method: "GET", // *GET, POST, PUT, DELETE, etc.
+  mode: "cors", // no-cors, *cors, same-origin
+});
+console.log(response);
+const Tours = await response.json();
+
+console.log(Tours);
+</script>
+
+<style scoped></style>
